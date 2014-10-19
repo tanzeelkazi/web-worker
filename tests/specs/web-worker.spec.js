6dae901bf4c30b03935f10fe2f07c9c1a07da4a2
Tanzeel Kazi <me@tanzeelkazi.com>
2014-10-19 10:35:19 -0500

    Update specs

--- tests/specs/web-worker.spec.js
+++ tests/specs/web-worker.spec.js
@@ -17,14 +17,31 @@
                 return;
             });
 
-            it("should accept string selector as first argument if the element exists", function () {
-                try {
-                    worker = new WebWorker("#test-worker-script-elem");
-                }
-                catch (err) {
-                }
+            describe("constructor", function () {
 
-                expect(worker).not.toBe(null);
+                it("should accept string selector if first argument is string and if the element exists", function () {
+                    try {
+                        worker = new WebWorker("#test-worker-script-elem");
+                    }
+                    catch (err) {
+                    }
+
+                    expect(worker).not.toBe(null);
+
+                    return;
+                });
+
+                it("should accept string URL if first argument is string and an element of same selector does not exist", function () {
+                    try {
+                        worker = new WebWorker("/js/example-worker");
+                    }
+                    catch (err) {
+                    }
+
+                    expect(worker).not.toBe(null);
+
+                    return;
+                });
 
                 return;
             });

482a34431c301b8e9674ea616b2655596b8a4781
Tanzeel Kazi <me@tanzeelkazi.com>
2014-10-18 22:17:41 -0500

    Setup jasmine test runner

---
+++ tests/specs/tk-worker.spec.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    'use strict';
+
+    describe("TKWorker", function () {
+
+        it("should be defined", function () {
+            expect(TKWorker).toBeDefined();
+            return;
+        });
+
+        return;
+    });
+
+    return;
+})();

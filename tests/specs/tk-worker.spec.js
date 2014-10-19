12abcd0666d2cfbf3145c89534fe80599254447c
Tanzeel Kazi <me@tanzeelkazi.com>
2014-10-18 23:14:58 -0500

    Update specs

--- tests/specs/tk-worker.spec.js
+++ tests/specs/tk-worker.spec.js
@@ -1,11 +1,38 @@
 ﻿(function () {
     'use strict';
 
+    var worker = null;
+
     describe("TKWorker", function () {
 
         it("should be defined", function () {
             expect(TKWorker).toBeDefined();
             return;
+        });
+
+        describe("instance", function () {
+            
+            afterEach(function () {
+                worker = null;
+                return;
+            });
+
+            it("should accept string selector as first argument if the element exists", function () {
+                try {
+                    worker = new TKWorker("#test-worker-script-elem");
+                }
+                catch (err) {
+                }
+
+                expect(worker).not.toBe(null);
+
+                return;
+            });
+
+            return;
+        });
+
+        describe("TKWorker static", function () {
         });
 
         return;

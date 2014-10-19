063f6a0ea71f8cb0424ab137a67a9a6a7885c6ab
Tanzeel Kazi <me@tanzeelkazi.com>
2014-10-18 22:12:17 -0500

    Initial commit of TKWorker

---
+++ js/tk-worker.js
@@ -0,0 +1,41 @@
+﻿(function (context, className) {
+    'use strict';
+
+    var TKWorker = null,
+
+        defaultContext = window,
+        defaultClassName = 'TKWorker',
+
+        Errors = null;
+
+    context = context || defaultContext;
+    className = className || defaultClassName;
+
+    TKWorker = context[className] || null;
+
+    if (TKWorker !== null) {
+        return;
+    }
+
+    TKWorker = function () {
+        return;
+    };
+
+    Errors = {
+        INVALID_ARGUMENTS: "Invalid arguments supplied to this function"
+    };
+    TKWorker.Errors = Errors;
+
+    TKWorker.noConflict = function (context, className) {
+        context = context || defaultContext;
+        className = className || defaultClassName;
+
+        context[className] = TKWorker;
+
+        return TKWorker;
+    };
+
+    TKWorker.noConflict(context, className);
+
+    return;
+})(window, 'TKWorker');

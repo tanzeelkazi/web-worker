66d465cc9e52ffd81bcf00b1a708768891a52d76
Tanzeel Kazi <me@tanzeelkazi.com>
2014-10-25 14:33:50 -0500

    Add generated documentation.

---
+++ docs/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "WebWorker"
+    ],
+    "modules": [
+        "WebWorker"
+    ],
+    "allModules": [
+        {
+            "displayName": "WebWorker",
+            "name": "WebWorker",
+            "description": "The WebWorker module"
+        }
+    ]
+} };
+});
